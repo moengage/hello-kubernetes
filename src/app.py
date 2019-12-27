212cd7ad6d68c44682a74185b28b7be65657b73a
iamsudip <iamsudip@programmer.net>
2019-12-28 02:25:04 +0530

    add pod details

--- src/app.py
+++ src/app.py
@@ -1,13 +1,26 @@
+import os
+
 from flask import Flask
 from flask import request
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    print('*' * 100)
-    print(request.headers)
-    print('*' * 100)
-    return "Hello Kubernetes!"
+    template = """
+    Hostname: {pod_name}
+    Namespace: {pod_namespace}
+    IP: {pod_ip}
+    {request_method} {request_path} {http_version}
+    """
+    response = template.format(
+            pod_name=os.getenv('POD_NAME'),
+            pod_namespace=os.getenv('POD_NAMESPACE'),
+            pod_ip=os.getenv('POD_IP'),
+            request_method=request.method,
+            request_path=request.path,
+            http_version=request.environ.get('SERVER_PROTOCOL'))
+    return '\n'.join([response, str(request.headers)])
 
