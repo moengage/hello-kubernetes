933c3542a3e8208075416d56fd321be23df20bee
iamsudip <iamsudip@programmer.net>
2019-12-28 02:41:45 +0530

    add templating

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from flask import request
+from flask import request, render_template
 
 
 app = Flask(__name__)
@@ -9,18 +9,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    template = """
-    Hostname: {pod_name}
-    Namespace: {pod_namespace}
-    IP: {pod_ip}
-    {request_method} {request_path} {http_version}
-    """
-    response = template.format(
-            pod_name=os.getenv('POD_NAME'),
-            pod_namespace=os.getenv('POD_NAMESPACE'),
-            pod_ip=os.getenv('POD_IP'),
-            request_method=request.method,
-            request_path=request.path,
-            http_version=request.environ.get('SERVER_PROTOCOL'))
-    return '\n'.join([response, str(request.headers)])
-
+    template = (
+        "Hostname: {pod_name}\n"
+        "Namespace: {pod_namespace}\n"
+        "IP: {pod_ip}\n"
+        "{request_method} {request_path} {http_version}\n"
+    )
+    pod_details = template.format(
+        pod_name=os.getenv('POD_NAME'),
+        pod_namespace=os.getenv('POD_NAMESPACE'),
+        pod_ip=os.getenv('POD_IP'),
+        request_method=request.method,
+        request_path=request.path,
+        http_version=request.environ.get('SERVER_PROTOCOL')
+    )
+    context = ''.join([pod_details, str(request.headers)])
+    return render_template('template.html', context=context)
