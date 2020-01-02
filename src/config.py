b63577c0da37af2b383c2c81fd7767ad975cedbe
iamsudip <iamsudip@programmer.net>
2020-01-02 16:19:50 +0530

    change secret

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ import os
 
 S3_POD_ROLE_BUCKET_NAME = os.environ.get('S3_POD_ROLE_BUCKET_NAME')
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
-SECRET_KEY = 'FOO'
+SECRET_KEY = 'foo'
 SECRET_VALUE = os.environ.get(SECRET_KEY)
